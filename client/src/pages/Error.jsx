8ef4afa8d92f3d3f9838dae264396797ae46a36c
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-08-24 22:18:24 +0300

    Error page style added

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 import img from '../assets/images/not-found.svg'
+import Wrapper from '../assets/wrappers/ErrorPage'
 
 export default function Error() {
   return (
-    <div className="full-page">
+    <Wrapper className="full-page">
       <div>
         <img src={img} alt="not found" />
         <h3>ohh! page not found</h3>
         <p>We can't seem to find the page you're looking for</p>
         <Link to="/">back home</Link>
       </div>
-    </div>
+    </Wrapper>
   )
 }
