c7040f2d36ef026b4c177fdf53bb35118b9d6c52
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-08-24 22:10:43 +0300

    Error page structure added

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import img from '../assets/images/not-found.svg'
 
 export default function Error() {
   return (
-    <div>Error</div>
+    <div className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+        <h3>ohh! page not found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link to="/">back home</Link>
+      </div>
+    </div>
   )
 }
