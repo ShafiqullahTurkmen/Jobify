1b66e7bdb463476bd1e6928e5370a03243f0d643
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-08-25 08:11:41 +0300

    Register Page structure added

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Logo } from '../components'
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+}
 
 export default function Register() {
+  const [values, setValues] = useState(initialState);
+  // global state and useNavigate
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  }
   return (
-    <div>Register</div>
+    <div className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo /> 
+        <h3>Login</h3>
+        {/* name input */}
+        <div className="form-row">
+          <label htmlFor="name" className='form-label'>name</label>
+          <input 
+            type="text" 
+            value={values.name} 
+            name="name" 
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+         <button type="submit" className="btn btn-block">submit</button>
+      </form>
+    </div>
   )
 }
