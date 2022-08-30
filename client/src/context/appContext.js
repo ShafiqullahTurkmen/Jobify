bc74769966889150f98b3ea311c5fbb4b8601ec1
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-08-30 17:51:47 +0300

    Global context setup added

---
+++ client/src/context/appContext.js
@@ -0,0 +1,28 @@
+import React, { useState, useReducer, useContext} from 'react';
+
+const initialState = {
+  isLoading: false,
+  showAlert: false,
+  alertText: "",
+  alertType: "",
+}
+
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+  const [state, setState] = useState(initialState);
+  
+  return (
+    <AppContext.Provider
+      value={{...state}}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+const useAppContext = () => {
+  return useContext(AppContext);
+}
+
+export { AppProvider, initialState, useAppContext }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from 'react'
 import { Logo, FormRow, Alert } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage';
+import { useAppContext } from '../context/appContext';
 
 const initialState = {
   name: "",
   email: "",
   password: "",
   isMember: true,
-  showAlert: false,
 }
 
 export default function Register() {
   const [values, setValues] = useState(initialState);
   // global state and useNavigate
+  const { isLoading, showAlert } = useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
@@ -31,7 +32,7 @@ export default function Register() {
       <form className="form" onSubmit={onSubmit}>
         <Logo />
         <h3>{values.isMember ? "Login" : "Register"}</h3>
-        {values.showAlert && <Alert />}
+        {showAlert && <Alert />}
 
         {/* name input */}
         {!values.isMember && (
