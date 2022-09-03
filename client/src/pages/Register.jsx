476d1cc66e4c080312b7ae95849009f0b246e971
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-09-03 07:09:09 +0300

    Display Alert Completed

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -13,19 +13,25 @@ const initialState = {
 export default function Register() {
   const [values, setValues] = useState(initialState);
   // global state and useNavigate
-  const { isLoading, showAlert } = useAppContext();
+  const { isLoading, showAlert, displayAlert } = useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   }
 
   const handleChange = (e) => {
-    console.log(e.target);
+    setValues({ ...values, [e.target.name]: e.target.value });
   }
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert();
+      return
+    }
+
+    console.log(values);
   }
   return (
     <Wrapper className="full-page">
