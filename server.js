3aa4bfa5573a98ef7bd396f71a37780205b9916a
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-09-04 08:01:10 +0300

    Basic Express and Nodemon added

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("server running ....");
+import express from 'express';
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome")
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is listening http://localhost:${port}`);
+})
+
+console.log("server running");
