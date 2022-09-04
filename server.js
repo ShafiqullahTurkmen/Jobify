840fc6a34537381f7823864649c9e4791f08d77e
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-09-04 08:49:50 +0300

    Dotenv added

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 
 app.get("/", (req, res) => {
