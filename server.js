c9b08f7fb2a0cd7edf852404af81ec48e726ffac
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-09-04 08:30:33 +0300

    Error Handler middleware added

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
 const app = express();
 
 app.get("/", (req, res) => {
+  // throw new Error("eooror")
   res.send("Welcome")
 })
 
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
