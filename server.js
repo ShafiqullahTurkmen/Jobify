7289c48752aeb8df163c859865ba81099be10780
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-09-04 08:13:59 +0300

    Not Found middleware added

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import notFoundMiddleware from './middleware/not-found.js';
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome")
 })
+
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 5000;
 
