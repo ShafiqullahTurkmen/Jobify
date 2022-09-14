1033758896ebb34cd660199a2b10589fe2cb4bf5
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-09-14 06:25:46 +0300

    Pass error to error handler

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,11 @@
 import User from '../modals/User.js';
 
-const register = async (req, res) => {
+const register = async (req, res, next) => {
   try {
     const user = await User.create(req.body);
     res.status(200).json({user});
   } catch (error) {
-    res.status(500).json({error});
+    next(error);
   }
 }
 
