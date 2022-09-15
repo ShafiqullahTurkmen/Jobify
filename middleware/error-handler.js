95c223f4de3fc1d361dc17960d53cfd77e5e978c
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-09-15 06:27:51 +0300

    missing field error added

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,7 +6,15 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
     msg: "Something went wrong, try again later"
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message;
+    defaultError.msg = Object.values(err.errors).map((item) => item.message).join(", ");
+  }
+
+  // res.status(defaultError.statusCode).json({ msg: err });
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 }
 
 export default errorHandlerMiddleware;
