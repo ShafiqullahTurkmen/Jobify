35063143db530f000e4374c925f40337801f27d4
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-09-17 10:11:52 +0300

    Custom api error added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,17 @@
 import User from '../modals/User.js';
 import { StatusCodes } from 'http-status-codes';
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
 
 const register = async (req, res, next) => {
   const { name, email, password } = req.body;
 
   if (!(name && email && password)) {
-    throw new Error("please provide all value");
+    throw new CustomAPIError("please provide all value");
   }
 
   const user = await User.create({ name, email, password });
