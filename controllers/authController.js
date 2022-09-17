be442adabd736c927b3f9b1b39ed6970aff8c67c
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-09-17 10:34:27 +0300

    error folder added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,25 +1,6 @@
 import User from '../modals/User.js';
 import { StatusCodes } from 'http-status-codes';
-class CustomAPIError extends Error {
-  constructor(message) {
-    super(message);
-  }
-}
-
-class BadRequestError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.BAD_REQUEST;
-  }
-}
-
-class NotFoundError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.NOT_FOUND;
-  }
-}
-
+import { BadRequestError, NotFoundError } from '../errors/index.js';
 
 const register = async (req, res, next) => {
   const { name, email, password } = req.body;
