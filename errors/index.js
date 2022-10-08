1c1d3bfec8d4061373e837f4eda263df96589c1a
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-10-08 21:22:52 +0300

    UnAuthenticatedError added

--- errors/index.js
+++ errors/index.js
@@ -1,4 +1,9 @@
 import BadRequestError from './bad-request.js';
 import NotFoundError from './not-found.js';
+import UnAuthenticatedError from './unauthenticated.js';
 
-export { BadRequestError, NotFoundError };
+export { 
+  BadRequestError,
+  NotFoundError,
+  UnAuthenticatedError
+ };
