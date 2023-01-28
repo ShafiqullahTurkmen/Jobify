1059e166bf4e4a42b9b67b7fdf98f2f16e1b34e5
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-01-28 21:10:46 +0300

    toggle dropdown functionality added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import Wrapper from '../assets/wrappers/Navbar';
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa'
 import { useAppContext } from '../context/appContext';
 import Logo from './Logo';
+import { useState } from 'react';
 export default function Navbar() {
   const { toggleSidebar } = useAppContext();
+  const [showLogout, setShowLogout] = useState(false);
   return (
     <Wrapper>
       <div className="nav-center">
@@ -15,12 +17,12 @@ export default function Navbar() {
           <h3 className="logo-text"> dashboard </h3>
         </div>
         <div className="btn-container">
-          <button type='button' className="btn" onClick={() => console.log("show/hide dropdown")}>
+          <button type='button' className="btn" onClick={() => setShowLogout(!showLogout)}>
             <FaUserCircle/>
             john
             <FaCaretDown/>
           </button>
-          <div className="dropdown show-dropdown">
+          <div className={showLogout ? `dropdown show-dropdown` : "dropdown"}>
             <button type='button' className="dropdown-btn" onClick={() => console.log("logout user")}>
               logout
             </button>
