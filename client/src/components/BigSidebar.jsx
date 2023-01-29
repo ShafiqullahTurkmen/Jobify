f8163966302ee1c2e252922c72af67ccbbf799d9
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-01-29 13:49:59 +0300

    Big sidebar structure complete

--- client/src/components/BigSidebar.jsx
+++ client/src/components/BigSidebar.jsx
@@ -1,9 +1,20 @@
-import Wrapper from '../assets/wrappers/BigSidebar'
+import Wrapper from '../assets/wrappers/BigSidebar';
+import { useAppContext } from '../context/appContext';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 export default function BigSidebar() {
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-      <div>BigSidebar</div>
+      <div className={showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"}>
+        <div className="content">
+          <header>
+            <Logo/>
+          </header>
+          <NavLinks />
+        </div>
+      </div>
 
     </Wrapper>
   )
