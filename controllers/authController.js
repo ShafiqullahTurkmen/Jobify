5a2cd9fc45f8757d2e0e202be0ab3ca88d7f8abf
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-01-29 19:04:00 +0300

    update user server completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,7 +51,24 @@ const login = async (req, res) => {
 }
 
 const updateUser = async (req, res) => {
-  res.send("updateUser user");
+  const { email, name, lastName, location } = req.body;
+  if (!(email && name && lastName && location)) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
+  await user.save();
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({ user, token, location: user.location})
+
+
 }
 
 export { register, login, updateUser }
--- models/User.js
+++ models/User.js
@@ -41,6 +41,9 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre("save", async function () {
+  console.log(this.modifiedPaths());
+
+  if (!this.isModified("password")) return
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
