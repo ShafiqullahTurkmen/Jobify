52ffb90f582e0abf8ce42c61f63eb9c65146c563
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-01-29 12:58:15 +0300

    small sidebar structure added

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,9 +1,22 @@
-import Wrapper from '../assets/wrappers/SmallSidebar'
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import liks from '../utils/links';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
 
 export default function SmallSidebar() {
   return (
     <Wrapper>
-      <div>SmallSidebar</div>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button type='button' className="close-btn" onClick={() => console.log("toggle side bar")}>
+            <FaTimes/>
+          </button>
+          <header><Logo/></header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
 
     </Wrapper>
   )
