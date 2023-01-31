2a19a26f9213270e0c5f84a45db05acdb0b79fbd
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-01-31 07:58:54 +0300

    Axios custom instance setup added

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -35,7 +35,13 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  axios.defaults.headers.common["Authorization"] = `Bearer ${state.token}`
+  // axios.defaults.headers.common["Authorization"] = `Bearer ${state.token}`
+  const authFetch = axios.create({
+    baseURL: "/api/v1",
+    headers: {
+      Authorization: `Bearer ${state.token}`
+    }
+  })
 
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT });
@@ -115,7 +121,7 @@ const AppProvider = ({ children }) => {
 
   const updateUser = async (currentUser) => {
     try {
-      const { data } = await axios.patch("/api/v1/auth/updateUser", currentUser);
+      const { data } = await authFetch.patch("/auth/updateUser", currentUser);
       console.log(data);
     } catch (error) {
       console.log(error.respose);
