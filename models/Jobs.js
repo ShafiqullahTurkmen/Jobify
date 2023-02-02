dc55ee0ca7c2d0d7d12297e109bee75ce632aa98
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-02-02 21:40:07 +0300

    Job schema created

---
+++ models/Jobs.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company"],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "pending", "declined"],
+      default: "pending",
+    },
+    jobType: {
+      type: String,
+      enum: ["full-time", "part-time", "remote", "internship"],
+      default: "full-time",
+    },
+    jobLocation: {
+      type: String,
+      default: "my city",
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Job", JobSchema);
