c69835a5613074831cdd180322f045736561b43c
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-02-02 21:56:58 +0300

    create job controller completed

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,18 @@
+import Job from '../models/Jobs.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
+
+
 const createJob = async (req, res) => {
-  res.send("create job")
+  const { position, company } = req.body;
+
+  if (!(position && company)) {
+    throw new BadRequestError("Please provide all values");
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  
+  res.status(StatusCodes.CREATED).json({ job })
 }
 
 const getAllJobs = async (req, res) => {
