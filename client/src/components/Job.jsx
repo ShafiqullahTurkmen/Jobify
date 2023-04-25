eafd5dc592c20c54809fbd98e074c1d3341e8470
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-04-25 19:24:30 +0300

    moment package added

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import moment from 'moment';
 
-export default function Job({company}) {
+export default function Job({company, createdAt}) {
+  let date = moment(createdAt).format('MMM Do, YYYY')
   return (
-    <h5>{company}</h5>
+    <div>
+      <h5>{company}</h5>
+      <h5>{date}</h5>
+
+    </div>
   )
 }
