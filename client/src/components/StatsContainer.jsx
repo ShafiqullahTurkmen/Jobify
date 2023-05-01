48d68976eb6f890da6c9c14ed752b22e6fece03a
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-05-01 22:11:41 +0300

    stats container  added

--- client/src/components/StatsContainer.jsx
+++ client/src/components/StatsContainer.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
-import StatsItem from './StatsItem'
+import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import StatsItem from './StatsItem';
+import Wrapper from '../assets/wrappers/StatsContainer';
 
 export default function StatsContainer() {
+
+  const { stats } = useAppContext();
+  const defaultStats = [
+    {
+      title: "pending application",
+      count: stats.pending || 0,
+      icon: <FaSuitcaseRolling />,
+      color: "#e9b949",
+      bcg: "#fcefc7"
+    },
+    {
+      title: "interviews scheduled",
+      count: stats.interview || 0,
+      icon: <FaCalendarCheck />,
+      color: "#647acb",
+      bcg: "#e0e8f9"
+    },
+    {
+      title: "jobs declined",
+      count: stats.declined || 0,
+      icon: <FaBug />,
+      color: "#d66a6a",
+      bcg: "#ffeeee"
+    },
+  ]
   return (
-    <>
-    <h1>StatsContainer</h1>
-    <StatsItem />
-    </>
+    <Wrapper>
+      {
+        defaultStats.map((item, i) => <StatsItem key={`${item}${i}`} {...item} />)
+      }
+    </Wrapper>
   )
 }
