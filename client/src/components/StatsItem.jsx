4b02fd76002703b8055f26a97743ac10c762eec3
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-05-01 22:16:38 +0300

    stats item added

--- client/src/components/StatsItem.jsx
+++ client/src/components/StatsItem.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Wrapper from '../assets/wrappers/StatItem';
 
-export default function StatsItem() {
+export default function StatsItem({ count, title, icon, color, bcg }) {
   return (
-    <div>StatsItem</div>
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className="count">{count}</span>
+        <div className="icon">{icon}</div>
+      </header>
+      <h5 className='title'>{title}</h5>
+    </Wrapper>
   )
 }
