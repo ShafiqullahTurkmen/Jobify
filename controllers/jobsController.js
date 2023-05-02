edd0d5bc33797de4c3b94001f906caaca3813ece
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-05-02 08:03:48 +0300

    Aggregation added based on date

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -81,7 +81,17 @@ const showStats = async (req, res) => {
     declined: stats.declined || 0
   };
 
-  let monthlyApplications = [];
+  let monthlyApplications = await Job.aggregate([
+    {$match: { createdBy: mongoose.Types.ObjectId(req.user.userId)}},
+    {
+      $group: {
+        _id: { year: {$year: "$createdAt"}, month: {$month: "$createdAt"} },
+        count: {$sum: 1}
+      }
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1}},
+    { $limit: 6}
+  ])
   res.status(StatusCodes.OK).json({defaultStats, monthlyApplications})
 };
 
