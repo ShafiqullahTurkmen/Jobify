6bd5d480270bc8b429c712a7b9a5734fa1b6433e
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-05-15 21:48:03 +0300

    attachCookie util added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,11 @@
-import User from '../models/User.js';
-import { StatusCodes } from 'http-status-codes';
-import { BadRequestError, NotFoundError, UnAuthenticatedError } from '../errors/index.js';
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+import {
+  BadRequestError,
+  NotFoundError,
+  UnAuthenticatedError,
+} from "../errors/index.js";
+import attachCookie from "../utils/attachCookie.js";
 
 const register = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -9,29 +14,30 @@ const register = async (req, res, next) => {
     throw new BadRequestError("please provide all value");
   }
 
-  const emailAlreadyExists = await User.findOne({email});
-  
+  const emailAlreadyExists = await User.findOne({ email });
+
   if (emailAlreadyExists) {
     throw new BadRequestError("Email already exists");
   }
-  
+
   const user = await User.create({ name, email, password });
   const token = user.createJWT();
+  attachCookie({ res, token });
   res.status(StatusCodes.CREATED).json({
     user: {
       email: user.email,
       lastName: user.lastName,
       location: user.location,
-      name: user.name
-    }, 
-    token
+      name: user.name,
+    },
+    token,
   });
-}
+};
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   if (!(email && password)) {
-    throw new BadRequestError("Please provide all values")
+    throw new BadRequestError("Please provide all values");
   }
 
   const user = await User.findOne({ email }).select("+password");
@@ -47,8 +53,9 @@ const login = async (req, res) => {
   }
   const token = user.createJWT();
   user.password = undefined;
-  res.status(StatusCodes.OK).json({ user, token, location: user.location})
-}
+  attachCookie({ res, token });
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
+};
 
 const updateUser = async (req, res) => {
   const { email, name, lastName, location } = req.body;
@@ -64,7 +71,8 @@ const updateUser = async (req, res) => {
   await user.save();
 
   const token = user.createJWT();
-  res.status(StatusCodes.OK).json({ user, token, location: user.location})
-}
+  attachCookie({ res, token });
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
+};
 
-export { register, login, updateUser }
+export { register, login, updateUser };
